57139feed9647c2172b94f114ac21ea13ea88320
Paul Alexander <paulalexander23@live.co.uk>
2019-06-23 21:35:22 +0100

    Fix tests. 5/6 tests passed.

--- tests.m
+++ tests.m
@@ -3,109 +3,92 @@ function tests = tests()
 end
 
 function testEvaluatingFunction1dFiniteDifference(testCase)
-    n = 2^8;
-    x = linspace(1/n,1,n)';
+    x = setup1dX(2^8);
     
     problemDeg = [1, 2];
-    
-    D = init_fd({x}, problemDeg, 2);
+    D = init_fd(x, problemDeg, 2);
     method = @(x,y,deg) diff_fd(x,y,deg,D,problemDeg);
-    [actual, expected] = problem1d(x,method);
+    
+    [actual, expected] = function1d(x,method);
+    
     verifyEqual(testCase,actual,expected,'RelTol',1e-3,'AbsTol',1e-4)
 end
 
 function testEvaluatingFunction1dPseudoSpectral(testCase)
-    n = 2^8;
-    x = linspace(1/n,1,n)';
+    x = setup1dX(2^8);
+    
     method = @diff_ps;
-    [actual, expected] = problem1d(x,method);
+    
+    [actual, expected] = function1d(x,method);
+    
     verifyEqual(testCase,actual,expected,'RelTol',1e-14,'AbsTol',1e-15)
 end
 
+function test2dFiniteDifference(testCase)
+    x = setup2dX(2^8);
+    
+    Y = cos(2*pi*x{1}) + cos(2*pi*x{2}');
+    Y = reshape(Y,[1, numel(Y)]);
+    
+    degree = [1, 0]';
+    
+    accuracy = 2;
+    D = init_fd(x,degree,accuracy);
+    
+    actual = cell2mat(diff_fd(x,Y,degree,D,degree));
+    expected = -2*pi*sin(2*pi*x{1}) .* ones(size(x{2}'));
+    
+    verifySize(testCase, actual, [2^8, 2^8])
+    verifyEqual(testCase, actual, expected, 'RelTol', 1e-3, 'AbsTol', 1e-4)
+end
+
 function testEvaluatingFunction2dFiniteDifference(testCase)
-    n = 2^8;
-    x = linspace(1/n,1,n)';
+    x = setup2dX(2^8);
     
     problemDeg = [1,0;0,1]';
     
-    D = init_fd({x}, problemDeg, 2);
+    D = init_fd(x, problemDeg, 2);
     method = @(x,y,deg) diff_fd(x,y,deg,D,problemDeg);
-    [actual, expected] = problem2d(x,method);
+    
+    [actual, expected] = function2d(x,method);
+    
+    verifySize(testCase, actual, [2^8, 2^8])
     verifyEqual(testCase,actual,expected,'RelTol',1e-3,'AbsTol',1e-4)
 end
 
 function testEvaluatingFunction2dPseudoSpectral(testCase)
-    n = 2^8;
-    x = linspace(1/n,1,n)';
+    x = setup2dX(2^8);
+    
     method = @diff_ps;
-    [actual, expected] = problem2d(x,method);
+    
+    [actual, expected] = function2d(x,method);
+    
     verifyEqual(testCase,actual,expected,'RelTol',1e-14,'AbsTol',1e-15)
-end
-
-function testEvaluatingFunction2dDivFiniteDifference(testCase)
-    n = 2^8;
-    x = linspace(1/n,1,n)';
-    
-    problemDeg = cat(3,[1,0;0,0],[0,0;0,1]);
-    
-    D = init_fd({x}, problemDeg, 2);
-    method = @(x,y,deg) diff_fd(x,y,deg,D,problemDeg);
-    [actual, expected] = problem2dDiv(x,method);
-    verifyEqual(testCase,actual,expected,'RelTol',1e-3,'AbsTol',1e-4)
-end
-
-function testEvaluatingFunction2dDivPseudoSpectral(testCase)
-    n = 2^8;
-    x = linspace(1/n,1,n)';
-    method = @diff_ps;
-    [actual, expected] = problem2dDiv(x,method);
-    verifyEqual(testCase,actual,expected,'RelTol',1e-14,'AbsTol',1e-15)
-end
-
-function problemMethod(testCase, problem, problemDeg, expectedFunction, diffMethod)
-    n = 2^8;
-    x = linspace(1/n,1,n)';
-    
-    y = cos(2*pi*x);
-    
-    if diffMethod == "diff_fd"
-        D = init_fd({x}, problemDeg, 2);
-        method = @(x,y,deg) diff_fd(x,y,deg,D,problemDeg);
-    elseif diffMethod == "diff_ps"
-        method = @diff_ps;
-    end
-    
-    actual = feval(problem,{x},y,method);
-    expected = expectedFunction(x);
-    
-    if diffMethod == "diff_fd"
-        verifyEqual(testCase,actual,expected,'RelTol',1e-3,'AbsTol',1e-4)
-    elseif diffMethod == "diff_ps"
-        verifyEqual(testCase,actual,expected,'RelTol',1e-14,'AbsTol',1e-15)
-    end
 end
 
 function [actual, expected] = function1d(x, method)
-    deg = [1, 2];
     y = cos(2 * pi * x{1});
-    dy = method(x, y, deg);
+    dy = method(x, y, [1, 2]);
+    
     actual = dy{2}/(4*pi^2) + dy{1}/(2*pi) + y;
-    expected = - sin(2 * pi * x);
+    expected = - sin(2 * pi * x{1});
 end
 
 function [actual, expected] = function2d(x,method)
-    deg = [1,0;0,1]';
-    y = cos(2 * pi * x{1}) + cos(2 * pi * x{2});
-    dy = method(x,y,deg);
-    actual = dy{1}/2/pi + dy{2}/pi;
-    expected = -sin(2 * pi * x{1}) - 2 * sin(2 * pi * x{2});
+    y = cos(2 * pi * x{1}) + cos(2 * pi * x{2}');
+    y = reshape(y,[1, numel(y)]);
+    
+    dy1 = cell2mat(method(x, y, [1, 0]'));
+    dy2 = cell2mat(method(x, y, [0, 1]'));
+    
+    actual = dy1/2/pi + dy2/2/pi;
+    expected = -sin(2 * pi * x{1}) - sin(2 * pi * x{2}');
 end
 
-function [actual, expected] = function2dDiv(x,method)
-    divdeg = cat(3,[1,0;0,0],[0,0;0,1]);
-    y = cos(2 * pi * x{1}) + cos(2 * pi * x{2});
-    dy = method(x,y,divdeg);
-    actual = dy{1} + dy{2};
-    expected = cat(3, - 2 * pi * sin(2 * pi * x{1}) * ones(size(x{2})), ...
-        - 2 * pi * sin(2 * pi * x{2}) * ones(size(x{1})));
+function x = setup1dX(n)
+    x = {linspace(1/n,1,n)'};
+end
+
+function x = setup2dX(n)
+    x = {linspace(1/n,1,n)';linspace(1/n,1,n)'};
 end
