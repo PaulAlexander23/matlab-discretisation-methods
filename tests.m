5b3e0cf03d55e70ec9d8a7a76570f37b8251bfd4
Paul Alexander <paulalexander23@live.co.uk>
2019-06-24 11:38:26 +0100

    Pass domain into functions rather than x and diff method.

--- tests.m
+++ tests.m
@@ -55,9 +55,7 @@ function testEvaluatingFunction1dFiniteDifference(testCase)
     
     domain = FDDomain(x, [1, 2], 2);
     
-    method = @(~, y, deg) domain.diff(y, deg);
-    
-    [actual, expected] = function1d(domain.x,method);
+    [actual, expected] = function1d(domain);
     
     verifyEqual(testCase,actual,expected,'RelTol',1e-3,'AbsTol',1e-4)
 end
@@ -67,9 +65,7 @@ function testEvaluatingFunction1dPseudoSpectral(testCase)
     
     domain = PSDomain(x);
     
-    method = @(~, y, deg) domain.diff(y, deg);
-    
-    [actual, expected] = function1d(domain.x,method);
+    [actual, expected] = function1d(domain);
     
     verifyEqual(testCase,actual,expected,'RelTol',1e-14,'AbsTol',1e-15)
 end
@@ -94,7 +90,7 @@ end
 function test2dPseudoSpectral(testCase)
     x = setup2dX(2^8);
     
-    domain = PDDomain(x, [1, 0]', 2);
+    domain = PSDomain(x);
     
     Y = cos(2*pi*domain.x{1}) + cos(2*pi*domain.x{2}');
     Y = reshape(Y,[1, numel(Y)]);
@@ -115,9 +111,7 @@ function testEvaluatingFunction2dFiniteDifference(testCase)
     
     domain = FDDomain(x, problemDeg, 2);
     
-    method = @(~, y, deg) domain.diff(y, deg);
-    
-    [actual, expected] = function2d(domain.x,method);
+    [actual, expected] = function2d(domain);
     
     verifySize(testCase, actual, [2^8, 2^8])
     verifyEqual(testCase,actual,expected,'RelTol',1e-3,'AbsTol',1e-4)
@@ -128,30 +122,28 @@ function testEvaluatingFunction2dPseudoSpectral(testCase)
     
     domain = PSDomain(x);
     
-    method = @(~, y, deg) domain.diff(y, deg);
-    
-    [actual, expected] = function2d(domain.x,method);
+    [actual, expected] = function2d(domain);
     
     verifyEqual(testCase,actual,expected,'RelTol',1e-14,'AbsTol',1e-15)
 end
 
-function [actual, expected] = function1d(x, method)
-    y = cos(2 * pi * x{1});
-    dy = method(x, y, [1, 2]);
+function [actual, expected] = function1d(domain)
+    y = cos(2 * pi * domain.x{1});
+    dy = domain.diff(y, [1, 2]);
     
     actual = dy{2}/(4*pi^2) + dy{1}/(2*pi) + y;
-    expected = - sin(2 * pi * x{1});
+    expected = - sin(2 * pi * domain.x{1});
 end
 
-function [actual, expected] = function2d(x,method)
-    y = cos(2 * pi * x{1}) + cos(2 * pi * x{2}');
+function [actual, expected] = function2d(domain)
+    y = cos(2 * pi * domain.x{1}) + cos(2 * pi * domain.x{2}');
     y = reshape(y,[1, numel(y)]);
     
-    dy1 = cell2mat(method(x, y, [1, 0]'));
-    dy2 = cell2mat(method(x, y, [0, 1]'));
+    dy1 = cell2mat(domain.diff(y, [1, 0]'));
+    dy2 = cell2mat(domain.diff(y, [0, 1]'));
     
     actual = dy1/2/pi + dy2/2/pi;
-    expected = -sin(2 * pi * x{1}) - sin(2 * pi * x{2}');
+    expected = -sin(2 * pi * domain.x{1}) - sin(2 * pi * domain.x{2}');
 end
 
 function x = setup1dX(n)
