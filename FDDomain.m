a3ec08428928369e08d0418a14a078e39f3977b8
Paul Alexander <paulalexander23@live.co.uk>
2019-06-24 17:40:03 +0100

    Neaten initialising the differentiation matrices.

--- FDDomain.m
+++ FDDomain.m
@@ -2,19 +2,19 @@ classdef FDDomain < Domain
     properties
         D
         diffDegrees
+        accuracy
     end
     
     methods
         function obj = FDDomain(x, diffDegrees, accuracy)
             obj = obj@Domain(x);
-            obj.D = init_fd(x, diffDegrees, accuracy);
-            obj.diffDegrees = diffDegrees;
             
-            initialiseDiffMatrices(obj, diffDegrees, accuracy);
+            obj.diffDegrees = diffDegrees;
+            obj.accuracy = accuracy;
+            obj.D = getDiffMatrices(obj, obj.diffDegrees, obj.accuracy);
         end
         
         function dy = diff(obj, y, deg)
-            
             dy = reshapeToVector(obj, y);
             
             index = findDiffCellIndex(obj, deg);
@@ -24,53 +24,62 @@ classdef FDDomain < Domain
             dy = reshapeToShape(obj, dy);
             
             dy = {dy};
-            
         end
     end
     
     methods (Access=private)
-        function Y = reshapeToVector(obj, y)
-            Y = reshape(y,prod(obj.shape),[]);
-        end
-        
-        function index = findDiffCellIndex(obj, deg)
-            [~,index] = ismember(deg(:,1)',obj.diffDegrees','rows');
-        end
-        
-        function y = reshapeToShape(obj, Y)
-            y = reshape(Y, [obj.shape,size(Y,2)]);
-        end
-        
-        function D = initialiseDiffMatrices(obj, diffDegrees, accuracy)
-            
-            dx = cellfun(@(x) x(2)-x(1),obj.x);
+        function D = getDiffMatrices(obj, diffDegrees, accuracy)
             
             D = cell(1,size(diffDegrees,2));
             
             for j = 1:size(diffDegrees,2)
                 D{j} = 1;
                 for k = 1:obj.dimension
-                    [s,c] = centred_scheme(diffDegrees(k,j), accuracy);
                     
-                    Dmat = construct_matrix(obj.shape(k),dx(k),s,c,diffDegrees(k,j));
+                    Dmat = constructMatrix(obj.x{k}, diffDegrees(k, j), accuracy);
                     
-                    D{j} = kron(Dmat,D{j});
+                    D{j} = kron(Dmat, D{j});
                 end
             end
             
-            function [s,c] = centred_scheme(degree, accuracy)
-                sL = ceil(degree/2) + ceil(accuracy/2)-1;
-                s = -sL:sL;
-                ex = (0:2*sL)';
-                S = repmat(s,sL*2+1,1).^ex;
-                del = factorial(degree) * (ex == degree);
-                c = (S\del)';
+            function mat = constructMatrix(x, degree, accuracy)
+                [stencil, coefficients] = centredScheme(degree, accuracy);
+                xLength = length(x);
+                dx = x(2)-x(1);
+                B = repmat(coefficients*dx.^(-degree),xLength,1);
+                mat = spdiagsPeriodicBCs(B, stencil, xLength);
             end
             
-            function mat = construct_matrix(nx,dx,s,c,d)
-                mat = spdiags(ones(nx,1)*c,s,nx,nx)*dx.^(-d);
-                mat = mat + spdiags(ones(nx,1)*c,s+nx,nx,nx)*dx.^(-d);
-                mat = mat + spdiags(ones(nx,1)*c,s-nx,nx,nx)*dx.^(-d);
+            function [stencil,coefficients] = centredScheme(degree, accuracy)
+                stencilLength = ceil(degree/2) + ceil(accuracy/2)-1;
+                stencil = -stencilLength:stencilLength;
+                ex = (0:2*stencilLength)';
+                S = repmat(stencil,stencilLength*2+1,1).^ex;
+                del = factorial(degree) * (ex == degree);
+                coefficients = (S\del)';
+            end
+            
+            function mat = spdiagsPeriodicBCs(B, stencil, size1)
+                mat = spdiags(B, stencil, size1, size1);
+                mat = spdiags(B, stencil + size1, size1, size1) + mat;
+                mat = spdiags(B, stencil - size1, size1, size1) + mat;
+            end
+            
+        end
+        
+        function Y = reshapeToVector(obj, y)
+            Y = reshape(y, prod(obj.shape), []);
+        end
+        
+        function index = findDiffCellIndex(obj, deg)
+            [~, index] = ismember(deg(:,1)', obj.diffDegrees', 'rows');
+        end
+        
+        function y = reshapeToShape(obj, Y)
+            if obj.dimension == 1
+                y = reshape(Y, [obj.shape(1), size(Y, 2)]);
+            else
+                y = reshape(Y, [obj.shape, size(Y, 2)]);
             end
         end
     end
