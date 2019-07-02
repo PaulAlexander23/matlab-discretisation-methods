94c221a13edea52c06765e8bee845c39e2d388be
Paul Alexander <paulalexander23@live.co.uk>
2019-07-02 17:42:13 +0100

    Separate boundary condition selection and add further tests.

--- FDDomain.m
+++ FDDomain.m
@@ -3,70 +3,90 @@ classdef FDDomain < Domain
         D
         diffDegrees
         accuracy
+        BCs
     end
-
+    
     methods
-        function obj = FDDomain(x, diffDegrees, accuracy)
+        function obj = FDDomain(x, diffDegrees, accuracy, BCs)
             obj = obj@Domain(x);
-
+            if nargin >= 4
+                obj.BCs = BCs;
+            else
+                obj.BCs = cell(obj.dimension, 1);
+                obj.BCs(:) = {"Periodic"};
+            end
             obj.diffDegrees = diffDegrees;
             obj.accuracy = accuracy;
-            obj.D = initialiseDiffMatrices(obj, obj.diffDegrees, obj.accuracy);
+            obj.D = initialiseDiffMatrices(obj, obj.diffDegrees);
         end
-
+        
         function dy = diff(obj, y, degree)
             dy = reshapeToVector(obj, y);
-
+            
             index = findDiffCellIndex(obj, degree);
-
+            
             dy = obj.D{index} * dy;
-
+            
             dy = reshapeToDomain(obj, dy);
         end
-
+        
         function D = getDiffMatrix(obj, deg)
             D = obj.D{findDiffCellIndex(obj, deg)};
         end
-    end
-
-    methods(Access = private)
-        function D = initialiseDiffMatrices(obj, diffDegrees, accuracy)
+        
+        function D = initialiseDiffMatrices(obj, diffDegrees)
             D = cell(1, size(diffDegrees, 2));
-
+            
             for j = 1:size(diffDegrees, 2)
                 D{j} = 1;
-                for k = 1:obj.dimension
-                    Dmat = constructMatrix(obj.x{k}, diffDegrees(k, j), accuracy);
+                for dim = 1:obj.dimension
+                    Dmat = obj.constructMatrix(obj.x{dim}, diffDegrees(dim, j), dim);
                     D{j} = kron(Dmat, D{j});
                 end
             end
-
-            function mat = constructMatrix(x, degree, accuracy)
-                [stencil, coefficients] = centredScheme(degree, accuracy);
-                xLength = length(x);
-                dx = x(2) - x(1);
-                B = repmat(coefficients*dx.^(-degree), xLength, 1);
-                mat = spdiagsPeriodicBCs(B, stencil, xLength);
-            end
-
-            function [stencil, coefficients] = centredScheme(degree, accuracy)
-                stencilLength = ceil(degree/2) + ceil(accuracy/2) - 1;
-                stencil = -stencilLength:stencilLength;
-                ex = (0:2 * stencilLength)';
-                S = repmat(stencil, stencilLength*2+1, 1).^ex;
-                del = factorial(degree) * (ex == degree);
-                coefficients = (S \ del)';
-            end
-
-            function mat = spdiagsPeriodicBCs(B, stencil, size1)
-                mat = spdiags(B, stencil, size1, size1);
-                mat = spdiags(B, stencil+size1, size1, size1) + mat;
-                mat = spdiags(B, stencil-size1, size1, size1) + mat;
-            end
         end
-
+        
         function index = findDiffCellIndex(obj, deg)
             [~, index] = ismember(deg(:, 1)', obj.diffDegrees', 'rows');
+        end
+        
+    end
+    
+    methods(Access = private)
+        function mat = constructMatrix(obj, x, degree, dim)
+            [stencil, coefficients] = obj.centredScheme(degree, obj.accuracy);
+            xLength = length(x);
+            dx = x(2) - x(1);
+            B = repmat(coefficients*dx.^(-degree), xLength, 1);
+            mat = spdiags(B, stencil, xLength, xLength);
+            mat = obj.addBoundaryConditions(mat, degree, dim, x, xLength);
+        end
+        
+        function mat = addBoundaryConditions(obj, mat, degree, dim, x, xLength)
+            if isstring(obj.BCs{dim})
+                if obj.BCs{dim} == "Periodic"
+                    [stencil, coefficients] = obj.centredScheme(degree, obj.accuracy);
+                    dx = x(2) - x(1);
+                    B = repmat(coefficients*dx.^(-degree), xLength, 1);
+                    mat = mat + obj.spdiagsPeriodicBCs(B, stencil, xLength);
+                end
+            else
+                mat = sparse(xLength,xLength);
+            end
+        end
+        
+        function [stencil, coefficients] = centredScheme(obj, degree, accuracy)
+            stencilLength = ceil(degree/2) + ceil(accuracy/2) - 1;
+            stencil = -stencilLength:stencilLength;
+            ex = (0:2 * stencilLength)';
+            S = repmat(stencil, stencilLength*2+1, 1).^ex;
+            del = factorial(degree) * (ex == degree);
+            coefficients = (S \ del)';
+        end
+        
+        function mat = spdiagsPeriodicBCs(obj, B, stencil, size1)
+            mat = spdiags(B, stencil+size1, size1, size1);
+            mat = spdiags(B, stencil-size1, size1, size1) + mat;
         end
     end
 end
--- tests.m
+++ tests.m
@@ -28,11 +28,24 @@ function testDomain2d(testCase)
     verifyEqual(testCase, domain.dimension, 2)
 end
 
-function test1dFiniteDifference(testCase)
-    x = setup1dX(2^8);
+function test1dCentralFiniteDifferenceMatrixConstruction(testCase)
+    x = {1:16};
+    domain = FDDomain(x, 1, 2);
+    actual = domain.getDiffMatrix(1);
+    B = [-0.5,0.5].*ones(16,1);
+    expected = spdiags(B, [-1,1], 16, 16);
+    expected(16,1) = 0.5;
+    expected(1,16) = -0.5;
+    verifyEqual(testCase, actual,expected);
+end
 
     domain = FDDomain(x, 1, 2);
 
+function test1dFiniteDifference(testCase)
+    x = setup1dX(2^8);
+    
+    domain = FDDomain(x, 1, 2);
+    
     Y = cos(2*pi*domain.x{1});
 
     degree = 1;
@@ -206,93 +219,92 @@ end
 
 function testEvaluatingFunction2dVectorisedFiniteDifference(testCase)
     x = setup2dX(2^8);
-
+    
     problemDeg = [1, 0; 0, 1]';
-
+    
     domain = FDDomain(x, problemDeg, 2);
-
+    
     [actual, expected] = function2dVectorised(domain);
-
+    
     verifySize(testCase, actual, [2^8, 2^8, 2])
     verifyEqual(testCase, actual, expected, 'RelTol', 1e-3, 'AbsTol', 1e-4)
 end
 
 function test1dBoundaryConditionsFiniteDifference(testCase)
-   x = setup1dX(2^8);
-   
-   c0 = [1, 0];
-   c1 = [0, 1];
-   f = [0, 0];
-   
-   domain = FDDomain(x, 1, 4, {[c0, c1, f]});
-   
-   y0 = domain.x{1} .* (domain.x{1} - 1).^3;
-   expected = (domain.x{1} - 1).^3 + 3 * domain.x{1}.^2 .* (domain.x{1} - 1).^2;
-   actual = domain.diff(y0, 1);
-   
-   verifyEqual(testCase, actual, expected, 'RelTol', 1e-3, 'AbsTol', 1e-4) 
+    x = setup1dX(2^8);
+    
+    c0 = [1, 0];
+    c1 = [0, 1];
+    f = [0, 0];
+    
+    domain = FDDomain(x, 1, 4, {[c0, c1, f]});
+    
+    y0 = domain.x{1} .* (domain.x{1} - 1).^3;
+    expected = (domain.x{1} - 1).^3 + 3 * domain.x{1}.^2 .* (domain.x{1} - 1).^2;
+    actual = domain.diff(y0, 1);
+    
+    verifyEqual(testCase, actual, expected, 'RelTol', 1e-3, 'AbsTol', 1e-4)
 end
 
 function test1dPeriodicBoundaryConditionsFiniteDifference(testCase)
-   x = setup1dX(2^8);
-   
-   domain = FDDomain(x, 1, 4, {'Periodic'});
-   
-   y0 = cos(2 * pi * domain.x{1});
-   expected = -2 * pi * sin(2 * pi * domain.x{1});
-   actual = domain.diff(y0, 1);
-   
-   verifyEqual(testCase, actual, expected, 'RelTol', 1e-3, 'AbsTol', 1e-4) 
+    x = setup1dX(2^8);
+    
+    domain = FDDomain(x, 1, 4, {"Periodic"});
+    
+    y0 = cos(2 * pi * domain.x{1});
+    expected = -2 * pi * sin(2 * pi * domain.x{1});
+    actual = domain.diff(y0, 1);
+    
+    verifyEqual(testCase, actual, expected, 'RelTol', 1e-3, 'AbsTol', 1e-4)
 end
 
 function test2dBoundaryConditionsFiniteDifference(testCase)
-   x = setup2dX(2^8);
-   
-   xc0 = [ones(2^8,1), zeros(2^8,1)];
-   xc1 = [zeros(2^8,1), zeros(2^8,1)];
-   xf = [zeros(2^8,1), zeros(2^8,1)];
-   yc0 = [ones(2^8,1), zeros(2^8,1)];
-   yc1 = [zeros(2^8,1), ones(2^8,1)];
-   yf = [zeros(2^8,1), zeros(2^8,1)];
-   
-   domain = FDDomain(x, [1, 0; 0, 1]', 4, {[xc0, xc1, xf], [yc0, yc1, yf]});
-   
-   y0 = (domain.x{1}.^2 /2 - domain.x{1}) .* (cos(2*pi * domain.x{2}') - 1);
-   expectedx = (cos(2*pi * domain.x{2}') - 1) .* (domain.x{1} - 1);
-   expectedy = -2*pi * sin(2*pi * domain.x{2}') .* (domain.x{1}.^2 /2 - domain.x{1});
-   
-   verifyEqual(testCase, domain.diff(y0, [1; 0]), expectedx, 'RelTol', 1e-3, 'AbsTol', 1e-4)
-   verifyEqual(testCase, domain.diff(y0, [0; 1]), expectedy, 'RelTol', 1e-3, 'AbsTol', 1e-4) 
+    x = setup2dX(2^8);
+    
+    xc0 = [ones(2^8,1), zeros(2^8,1)];
+    xc1 = [zeros(2^8,1), zeros(2^8,1)];
+    xf = [zeros(2^8,1), zeros(2^8,1)];
+    yc0 = [ones(2^8,1), zeros(2^8,1)];
+    yc1 = [zeros(2^8,1), ones(2^8,1)];
+    yf = [zeros(2^8,1), zeros(2^8,1)];
+    
+    domain = FDDomain(x, [1, 0; 0, 1]', 4, {[xc0, xc1, xf], [yc0, yc1, yf]});
+    
+    y0 = (domain.x{1}.^2 /2 - domain.x{1}) .* (cos(2*pi * domain.x{2}') - 1);
+    expectedx = (cos(2*pi * domain.x{2}') - 1) .* (domain.x{1} - 1);
+    expectedy = -2*pi * sin(2*pi * domain.x{2}') .* (domain.x{1}.^2 /2 - domain.x{1});
+    
+    verifyEqual(testCase, domain.diff(y0, [1; 0]), expectedx, 'RelTol', 1e-3, 'AbsTol', 1e-4)
+    verifyEqual(testCase, domain.diff(y0, [0; 1]), expectedy, 'RelTol', 1e-3, 'AbsTol', 1e-4)
 end
 
 function test2dPeriodicBoundaryConditionsFiniteDifference1(testCase)
-   x = setup2dX(2^8);
-   
-   xc0 = [ones(2^8,1), zeros(2^8,1)];
-   xc1 = [zeros(2^8,1), zeros(2^8,1)];
-   xf = [zeros(2^8,1), zeros(2^8,1)];
-   
-   domain = FDDomain(x, [1, 0; 0, 1]', 4, {[xc0, xc1, xf], 'Periodic'});
-   
-   y0 = (domain.x{1}.^2 /2 - domain.x{1}) .* cos(2*pi * domain.x{2}');
-   expectedx = cos(2*pi * domain.x{2}') .* (domain.x{1} - 1);
-   expectedy = -2*pi * sin(2*pi * domain.x{2}') .* (domain.x{1}.^2 /2 - domain.x{1});
-   
-   verifyEqual(testCase, domain.diff(y0, [1; 0]), expectedx, 'RelTol', 1e-3, 'AbsTol', 1e-4)
-   verifyEqual(testCase, domain.diff(y0, [0; 1]), expectedy, 'RelTol', 1e-3, 'AbsTol', 1e-4) 
+    x = setup2dX(2^8);
+    
+    xc0 = [ones(2^8,1), zeros(2^8,1)];
+    xc1 = [zeros(2^8,1), zeros(2^8,1)];
+    xf = [zeros(2^8,1), zeros(2^8,1)];
+    
+    domain = FDDomain(x, [1, 0; 0, 1]', 4, {[xc0, xc1, xf], "Periodic"});
+    
+    y0 = (domain.x{1}.^2 /2 - domain.x{1}) .* cos(2*pi * domain.x{2}');
+    expectedx = cos(2*pi * domain.x{2}') .* (domain.x{1} - 1);
+    expectedy = -2*pi * sin(2*pi * domain.x{2}') .* (domain.x{1}.^2 /2 - domain.x{1});
+    
+    verifyEqual(testCase, domain.diff(y0, [1; 0]), expectedx, 'RelTol', 1e-3, 'AbsTol', 1e-4)
+    verifyEqual(testCase, domain.diff(y0, [0; 1]), expectedy, 'RelTol', 1e-3, 'AbsTol', 1e-4)
 end
 
 function test2dPeriodicBoundaryConditionsFiniteDifference2(testCase)
-   x = setup2dX(2^8);
-   
-   domain = FDDomain(x, [1, 0; 0, 1]', 4, {'Periodic', 'Periodic'});
-   
-   y0 = cos(2*pi * domain.x{1}) .* cos(2*pi * domain.x{2}');
-   expectedx = cos(2*pi * domain.x{2}') .* -2*pi * sin(2*pi * domain.x{1});
-   expectedy = -2*pi * sin(2*pi * domain.x{2}') .* cos(2*pi * domain.x{1});
-   
-   verifyEqual(testCase, domain.diff(y0, [1; 0]), expectedx, 'RelTol', 1e-3, 'AbsTol', 1e-4)
-   verifyEqual(testCase, domain.diff(y0, [0; 1]), expectedy, 'RelTol', 1e-3, 'AbsTol', 1e-4) 
+    x = setup2dX(2^8);
+    
+    domain = FDDomain(x, [1, 0; 0, 1]', 4, {"Periodic", "Periodic"});
+    
+    y0 = cos(2*pi * domain.x{1}) .* cos(2*pi * domain.x{2}');
+    expectedx = cos(2*pi * domain.x{2}') .* -2*pi .* sin(2*pi * domain.x{1});
+    expectedy = -2*pi * sin(2*pi * domain.x{2}') .* cos(2*pi * domain.x{1});
+    verifyEqual(testCase, domain.diff(y0, [1; 0]), expectedx, 'RelTol', 1e-3, 'AbsTol', 1e-4)
+    verifyEqual(testCase, domain.diff(y0, [0; 1]), expectedy, 'RelTol', 1e-3, 'AbsTol', 1e-4)
 end
 
 function x = setup1dX(n)
