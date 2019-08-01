99cd418f380fdb73c527e7e6aa2c1c0759c4f3b9
Paul Alexander <paulalexander23@live.co.uk>
2019-08-01 15:52:08 +0100

    Remove extra files.

--- trunc.m
+++
@@ -1,8 +0,0 @@
-function v = trunc(u, r)
-    %TRUNC Truncate vector or matrix in the first dimension
-    %   u - vector or matrix
-    %   r - truncation ratio
-    N = size(u,1);
-    ind = [1:r * N/2, N * (1 - r/2) + 1:N];
-    v = u(ind,:);
-end
--- zeropad.m
+++
@@ -1,9 +0,0 @@
-function v = zeropad(u, r)
-    %ZEROPAD Zero pad vector or matrix in the first dimension
-    %   u - vector or matrix
-    %   r - extention ratio
-    [N, M] = size(u);
-    v = zeros(N * r, M);
-    ind = [1:N/2, N * (r - 0.5) + 1:N * r];
-    v(ind,:) = u;
-end
